9a4b8b9ca883e37b64065dd947e208878d916132
VictoriaCardos <vvjcerque@hotmail.com>
2021-12-31 14:46:19 -0300

    feat: using state for change num

--- fundamentos-react/src/components/comunicacao/indireta/Super.jsx
+++ fundamentos-react/src/components/comunicacao/indireta/Super.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Sub from './Sub'
 
 export default props => {
+  const [num, setNum] = useState(0)
+
   function handleClick(value) {
-    console.log('Ação!!!')
-    console.log(value)
+    setNum(value)
   }
 
   return (
     <div>
-      <h4>Valor</h4>
+      <h4>Valor: {num}</h4>
       <Sub handleClick={handleClick}></Sub>
     </div>
   )
