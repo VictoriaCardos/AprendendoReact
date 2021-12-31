991d70241ab901f044b090649f53728f2b5a0372
VictoriaCardos <vvjcerque@hotmail.com>
2021-12-30 23:48:59 -0300

    feat: add news cards

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -8,22 +8,27 @@ import ComParametro from './components/basicos/ComParametro'
 import Repeticao from './components/basicos/Repeticao'
 import Condicional from './components/basicos/Condicional'
 import CondicionalComIf from './components/basicos/CondicionalComIf'
+import Pai from './components/comunicacao/direta/Pai'
 
 export default props => (
   <div className="App">
-    <Card titulo="#06 - Condicional V2">
+    <Card titulo="#07 - Comunicação direta" color="#4298B5">
+      <Pai sobrenome="Cardoso" />
+    </Card>
+
+    <Card titulo="#06 - Condicional V2" color="#FA6900">
       <CondicionalComIf numero={11} />
     </Card>
 
-    <Card titulo="#05 - Condicional V1">
+    <Card titulo="#05 - Condicional V1" color="#E94C6F">
       <Condicional numero={10} />
     </Card>
 
-    <Card titulo="#04 - Componente Com Filhos">
+    <Card titulo="#04 - Componente Com Filhos" color="#008BBA">
       <Repeticao />
     </Card>
 
-    <Card titulo="#03 - Componente Com Filhos">
+    <Card titulo="#03 - Componente Com Filhos" color="#D96459">
       <ComFilhos>
         <ul>
           <li>Ana</li>
@@ -34,11 +39,11 @@ export default props => (
       </ComFilhos>
     </Card>
 
-    <Card titulo="#02 - Componente Com Parametro">
+    <Card titulo="#02 - Componente Com Parametro" color="#FF85CB">
       <ComParametro titulo="Esse é o titulo" subtitulo="Esse é o subtitulo" />
     </Card>
 
-    <Card titulo="#01 - Primeiro Componente">
+    <Card titulo="#01 - Primeiro Componente" color="#92B06A">
       <Primeiro />
     </Card>
   </div>
