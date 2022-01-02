24b99752d12202a6fc240cf9701bf224a82e2507
VictoriaCardos <vvjcerque@hotmail.com>
2022-01-01 23:54:56 -0300

    fix: fixed name of function and add htmlFor in label

--- fundamentos-react/src/components/contador/PassoForm.jsx
+++ fundamentos-react/src/components/contador/PassoForm.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 export default props => {
   return (
     <div>
-      <label for="passoInput">Passo: </label>
+      <label htmlFor="passoInput">Passo: </label>
       <input
         id="passoInput"
         type="number"
         value={props.passo}
-        onChange={e => props.OnPassoChange(+e.target.value)}
+        onChange={e => props.onPassoChange(+e.target.value)}
         style={{ fontSize: '1.4rem', width: '60px' }}
       />
     </div>
