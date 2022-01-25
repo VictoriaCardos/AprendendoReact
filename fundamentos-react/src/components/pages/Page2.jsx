26a07e4a78efa7daf3e1376e767e08070bd8986f
VictoriaCardos <vvjcerque@hotmail.com>
2022-01-25 20:48:55 -0300

    feat: add button for go back

--- fundamentos-react/src/components/pages/Page2.jsx
+++ fundamentos-react/src/components/pages/Page2.jsx
@@ -1,17 +1,16 @@
 import React from 'react'
 import Button from './Button'
+import Card from '../layout/Card'
 
 const Page2 = props => {
   function goBack() {
     props.history.goBack()
   }
   return (
-    <>
-      <div style={{ background: 'red' }}>
-        <h1>PAGINA 2</h1>
-        <Button onClick={goBack}>Voltar</Button>
-      </div>
-    </>
+    <Card titulo="#page 2" color="red">
+      <h1>PAGINA 2</h1>
+      <Button onClick={goBack}>Voltar</Button>
+    </Card>
   )
 }
 
